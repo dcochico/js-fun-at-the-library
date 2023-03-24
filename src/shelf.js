cd7dfcc52f77c9ea7037600cd264b53de3cd7aca
Daniel Cochico <dcochico.20@gmail.com>
2023-03-23 19:40:07 -0700

    Complete Shelf Exercises

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,74 @@
+var hyperion = {
+  title: "Hyperion",
+  mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
+  pageCount: 482,
+  genre: "sciFi"
+};
+var dune = {
+  title: "Dune",
+  mainCharacter: { name: "Paul", age: 15, pronouns: "he/him" },
+  pageCount: 421,
+  genre: "sciFi"
+};
+var endersGame = {
+  title: "Ender's Game",
+  mainCharacter: { name: "Andrew 'Ender' Wiggin", age: 10, pronouns: "he/him" },
+  pageCount: 324,
+  genre: "sciFi"
+};
+var sciFiShelf = [hyperion, dune, endersGame];
+
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+  return shelf;
+}
+
+
+
+
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookTitle) {
+      shelf.splice(i,1);
+    }
+  }
+}
+
+
+
+
+
+function listTitles(shelf) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+  }
+  return titles.join(', ');
+}
+
+
+
+
+
+function searchShelf(shelf, bookTitle) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title.includes(bookTitle)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
